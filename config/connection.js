b52fa67edfeecc16daf9690b4184ae159cf70f51
asharma1398 <asharma1398@gmail.com>
2020-07-16 13:11:32 -0400

    adding to heroku

--- config/connection.js
+++ config/connection.js
@@ -13,6 +13,18 @@ var connection = mysql.createConnection({
     database: "pizzaDB"
   });
 
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: process.env.DB_PASS,
+//     database: "pizzaDB"
+//   });
+// }
+
 // MYSQL CONNECTION FUNCTION
 // =========================================================================
 connection.connect(function(err) {
