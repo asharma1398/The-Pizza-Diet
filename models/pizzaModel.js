0ae2d2c85f9d0523779c2a0acef87759d70b7e34
asharma1398 <asharma1398@gmail.com>
2020-07-16 00:51:52 -0400

    objects added to orm and functions created in models

--- models/pizzaModel.js
+++ models/pizzaModel.js
@@ -2,8 +2,23 @@
 var orm = require("../config/orm.js");
 
 var pizza = {
-
-};
+    all: function(cb) {
+      orm.all("pizza", function(res) {
+        cb(res);
+      });
+    },
+    
+    create: function(cols, vals, cb) {
+      orm.create("pizza", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    update: function(objColVals, condition, cb) {
+      orm.update("pizza", objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+  };
 
 // Export the database functions for the controller (pizzaController.js).
 module.exports = pizza;
