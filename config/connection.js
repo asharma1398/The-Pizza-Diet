816dbf8a11732efe88fb15594fc85d19fcaaa9b4
asharma1398 <asharma1398@gmail.com>
2020-07-15 22:58:45 -0400

    connection to mysql complete

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,27 @@
 // DEPENDENCIES 
 // =========================================================================
 const mysql = require("mysql");
+require("dotenv").config();
+
+// MYSQL CONNECTION
+// =========================================================================
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.DB_PASS,
+    database: "pizzaDB"
+  });
+
+// MYSQL CONNECTION FUNCTION
+// =========================================================================
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+
+// Export connection for our ORM to use.
+module.exports = connection;
